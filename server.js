1564cc4c87ee800b25cc0ed7db144c1fc7215c8a
Thomas Phillips <tpphilli@amazon.com>
2016-03-06 10:51:12 -0800

    Created first beer model

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var mongoose = require ('mongoose');
+var Beer = require('./models/beer');
+
 var app = express();
 var port = process.env.PORT || 3000;
 var router = express.Router();
+
+mongoose.connect('mongodb://localhost:27017/beerlocker');
 
 router.get('/', function(req, res) {
   res.json({message: 'You are running low on beer'});
