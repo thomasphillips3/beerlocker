8774141c1dac22a7e72dbcf6ac2e9876b3224401
Thomas Phillips <tpphilli@amazon.com>
2016-03-17 21:28:54 -0700

    Added authentication to methods.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 var beerController = require('./controllers/beer');
 var userController = require('./controllers/user');
 var Beer = require('./models/beer');
+var passport = require('passport');
+var authController = require('./controllers/auth');
 var app = express();
 var port = process.env.PORT || 3000;
 var router = express.Router();
@@ -15,19 +17,20 @@ mongoose.connect('mongodb://localhost:27017/beerlocker');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(passport.initialize());
 
 router.route('/beers')
-  .post(beerController.postBeers)
-  .get(beerController.getBeers);
+  .post(authController.isAuthenticated, beerController.postBeers)
+  .get(authController.isAuthenticated, beerController.getBeers);
 
 router.route('/beers/:beer_id')
-  .get(beerController.getBeer)
-  .put(beerController.putBeer)
-  .delete(beerController.deleteBeer);
+  .get(authController.isAuthenticated, beerController.getBeer)
+  .put(authController.isAuthenticated, beerController.putBeer)
+  .delete(authController.isAuthenticated, beerController.deleteBeer);
 
 router.route('/users')
   .post(userController.postUsers)
-  .get(userController.getUsers);
+  .get(authController.isAuthenticated, userController.getUsers);
 
 app.use('/api', router);
 app.listen(port);
