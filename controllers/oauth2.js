aecade81ea397a545651b65a9c005c0ad005205a
Thomas Phillips <tpphilli@amazon.com>
2016-03-20 02:42:15 -0700

    User authorization endpoint

--- controllers/oauth2.js
+++ controllers/oauth2.js
@@ -55,3 +55,16 @@ server.exchange(oauth2orize.exchange.code(function(client, code, redirectUri, ca
     });
   });
 }));
+
+exports.authorization = [
+  server.authorization(function(clientId, redirectUri, callback) {
+    Client.findOne({ id: clientId }, function (err, client) {
+      if (err) { return callback(err); }
+
+      return callback(null, client, redirectUri);
+    });
+  }),
+  function(req, res){
+    res.render('dialog', { transactionID: req.oauth2.transactionID, user: req.user, client: req.oauth2.client });
+  }
+]
